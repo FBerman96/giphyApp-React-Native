e223558cd3dcccde7a9d09840b3e111c1c5c1fe2
FBerman96 <fernando.faour@davinci.edu.ar>
2022-03-02 15:58:46 -0300

    giphy APP

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, StyleSheet, FlatList, Image} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [gifs, setGifs] = useState([]);
+  const [term, updateTerm] = useState('');
+async function fetchGifs() {
+    try {
+      const API_KEY = 'xQQtrmOPbSryHAhCkfi6W2m7HLZSbUSd';
+      const BASE_URL = 'http://api.giphy.com/v1/gifs/search';
+      const resJson = await fetch(`${BASE_URL}?api_key=${API_KEY}&q=${term}&limit=15&offset=0&rating=pg&lang=en
+      `);
+      const res = await resJson.json();
+      setGifs(res.data);
+    } catch (error) {
+      console.warn(error);
+    }
+  } 
+function onEdit(newTerm) {
+    updateTerm(newTerm);
+    fetchGifs();
+  }
+return (
+    <View style={styles.view}>
+      <TextInput
+        placeholder="BUSCAR GIFS"
+        style={styles.textInput}
+        onChangeText={(text) => onEdit(text)}
+      />
+      <FlatList
+        data={gifs}
+        
+        renderItem={({item}) => (
+          <Image
+            resizeMode='contain'
+            style={styles.image}
+            source={{uri: item.images.original.url}}
+          />
+        )}
+        key={gifs.id}
+      />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#4AB021',
+  },
+  textInput: {
+    width: '100%',
+    height: 50,
+    color: 'orange',
+    alignItems: 'center',
+    margin: 2,
+  },
+  image: {
+    width: 300,
+    height: 150,
+    borderWidth: 3,
+    marginBottom: 5,
   },
 });
